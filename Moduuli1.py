c4313cc5eeaddc5d1d87ebea4173abab949ceaa0
Khale <khaled.hussein@metropolia.fi>
2023-08-30 11:46:54 +0300

    M1

---
+++ Moduuli1.py
@@ -0,0 +1,5 @@
+kerrat = int(input("Montako kertaa tervehditään: "))
+tehdyt = 0
+while tehdyt<kerrat:
+    print("Hyvää huomenta")
+    tehdyt = tehdyt + 1
---
+++ module-1.py
@@ -0,0 +1,4 @@
+lopet-toistot = False
+# while not lopeta-toistot
+if uusi_luku =int(syote
+if uusi_luku >pienin:
